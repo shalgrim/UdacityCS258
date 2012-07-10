ef94e09561817c258eab9dee1eb86cba34251374
shalgrim <shalgrim@fire>
2012-07-10 14:04:24 -0700

    seems like this works but the website no likey

--- Unit3/Unit3/q14.py
+++ Unit3/Unit3/q14.py
@@ -18,7 +18,37 @@
 # is_luhn_valid takes a credit card number as input and verifies 
 # whether it is valid or not. If it is valid, it returns True, 
 # otherwise it returns False.
+
+import copy
+
 def is_luhn_valid(n):
     ###Your code here.
-    pass
+    strn = str(n)
+    origDigs = [int(dig) for dig in strn]
+    assert int(''.join([str(d) for d in origDigs])) == n
 
+    modDigs = copy.copy(origDigs)
+    for i in range(len(modDigs)-2, -1, -2):
+        #print 'i: %d'%(i)
+        modDigs[i] = (modDigs[i]*2)%10
+    #print modDigs
+    total = sum(modDigs)
+
+    if total%10 == 0:
+        return True
+    else:
+        return False
+
+
+#print 0, is_luhn_valid(0)
+#print 5, is_luhn_valid(5)
+#print 50, is_luhn_valid(50)
+#print 55, is_luhn_valid(55)
+#print 999, is_luhn_valid(999)
+#print 500, is_luhn_valid(500)
+#print 505, is_luhn_valid(505)
+#print 5000, is_luhn_valid(5000)
+#print 5005, is_luhn_valid(5005)
+#print 50000, is_luhn_valid(50000)
+
+#raw_input()
